bc8f4bad1d4ea7591fcc751cc5f49106b2e4f95f
agelemerov <angel.gelemerov@gmail.com>
2022-10-17 10:12:41 +0100

    answered q1 and q2

---
+++ practice/practice1.py
@@ -0,0 +1,20 @@
+# 1.
+
+# correct syntax for defining a function
+def function():
+    pass
+
+
+# 2.
+
+list_items = [1, 2, 3, 4, 5]
+
+for i in list_items:
+    print(i)
+
+print("\nmiddle\n")
+
+limit = 0
+while limit < 5:
+    print(list_items[limit])
+    limit += 1
